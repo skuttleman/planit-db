c9c001ae4919f8b152887be676f1d295f776e6ff
Ben Allred <skuttleman@gmail.com>
2016-01-11 11:40:07 -0700

    seed batllama with skills

--- seeds/fill_type_tables.js
+++ seeds/fill_type_tables.js
@@ -1,32 +1,34 @@
 
 exports.seed = function(knex, Promise) {
-  return Promise.all([
-    knex('roles').del(),
-    knex('planit_types').del(),
-    knex('skills').del()
-  ]).then(function() {
+  if (false) {
     return Promise.all([
-      knex('roles').insert([
-        { name: 'normal' },
-        { name: 'moderator' },
-        { name: 'admin' }
-      ]),
-      knex('planit_types').insert([
-        { name: 'wedding' },
-        { name: 'bussiness conference' },
-        { name: 'charity banquet' },
-        { name: 'political rally' },
-        { name: 'gladiator fire arena of death' }
-      ]),
-      knex('skills').insert([
-        { name: 'administration' },
-        { name: 'juggler' },
-        { name: 'musician' },
-        { name: 'caterer' },
-        { name: 'florist' },
-        { name: 'dj' },
-        { name: 'manual labor' }
-      ])
-    ]);
-  });
+      knex('roles').del(),
+      knex('planit_types').del(),
+      knex('skills').del()
+    ]).then(function() {
+      return Promise.all([
+        knex('roles').insert([
+          { name: 'normal' },
+          { name: 'moderator' },
+          { name: 'admin' }
+        ]),
+        knex('planit_types').insert([
+          { name: 'wedding' },
+          { name: 'bussiness conference' },
+          { name: 'charity banquet' },
+          { name: 'political rally' },
+          { name: 'gladiator fire arena of death' }
+        ]),
+        knex('skills').insert([
+          { name: 'administration' },
+          { name: 'juggler' },
+          { name: 'musician' },
+          { name: 'caterer' },
+          { name: 'florist' },
+          { name: 'dj' },
+          { name: 'manual labor' }
+        ])
+      ]);
+    });
+  }
 };
