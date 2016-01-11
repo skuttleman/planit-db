85e636127c7b95fd9aca5af6d8e4efd37473fad7
Ben Allred <skuttleman@gmail.com>
2016-01-11 11:28:45 -0700

    seed for elana skills

---
+++ seeds/batllama_skills.js
@@ -0,0 +1,15 @@
+
+exports.seed = function(knex, Promise) {
+  return Promise.all([
+    knex('skills'),
+    knex('members').where('display_name', 'like', '%Elana%')
+  ]).then(function(data) {
+    var elana = data[1][0];
+    var skills = data[0];
+    return knex('member_skills').insert([
+      { member_id: elana.id, skill_id: skills[0].id },
+      { member_id: elana.id, skill_id: skills[1].id },
+      { member_id: elana.id, skill_id: skills[2].id }
+    ]);
+  });
+};
